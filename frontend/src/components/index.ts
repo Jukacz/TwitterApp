6942cd0e2b3e11248956d616e0e02e87b0ae8041
Smieluuu <kacper.smielak@wp.pl>
2023-04-13 13:51:44 +0200

    append tweet input form

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,4 +2,5 @@ import JoiInput from "./JoiInput/JoiInput";
 import Sidebar from "./Sidebar/Sidebar";
 import Post from "./Post/Post";
 import RightSidebar from "./RightSidebar/RightSidebar";
-export { Sidebar, JoiInput, Post, RightSidebar };
+import TweetForm from "./Tweet/TweetForm";
+export { Sidebar, JoiInput, Post, RightSidebar, TweetForm };
--- frontend/src/views/Dashboard/Dashboard.tsx
+++ frontend/src/views/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import "./Dashboard.scss";
 import React from "react";
-import { Sidebar, Post, RightSidebar } from "../../components";
+import { Sidebar, Post, RightSidebar, TweetForm } from "../../components";
 import { PostProps } from "../../components/Post/Post";
 
 const Dashboard: React.FC = () => {
@@ -18,6 +18,7 @@ const Dashboard: React.FC = () => {
     "@ogrodas",
     "@pingwinek2115",
     "@frontendnawypasie.com",
+    "@reactjs",
   ];
 
   const postProps: PostProps = {
@@ -28,10 +29,21 @@ const Dashboard: React.FC = () => {
     likes: likes,
   };
 
+  const handleTweetSubmit = (tweetText: string) => {
+    // do something with the tweet text
+    console.log(tweetText);
+  };
+
   return (
     <div className="dashboard-container">
       <Sidebar />
-      <Post className="post" {...postProps} />
+      <div>
+        <TweetForm
+          onTweetSubmit={handleTweetSubmit}
+          avatarUrl="https://media.gettyimages.com/id/1322571825/photo/robert-lewandowski-of-poland-poses-during-the-official-uefa-euro-2020-media-access-day-on.jpg?s=612x612&w=gi&k=20&c=uQjS0WUHg9EY-t3ghuvm_n_oJUiyDFPaE6IBC1IRRvo="
+        />
+        <Post className="post" {...postProps} />
+      </div>
       <RightSidebar hashtags={hashtags} nonFollowedUsers={nonFollowedUsers} />
     </div>
   );
